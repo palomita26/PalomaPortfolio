3a497370be762d50022e3cf465a68be3613ce532
MountainGurl <palomita@Palomas-MacBook-Air.local>
2023-10-09 20:06:36 -0400

    Paloma Land Header

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <h1 className= "mx-auto text-3xl"> 
+          {"Paloma Land"}
+        </h1>
       </div>
     </main>
   )
