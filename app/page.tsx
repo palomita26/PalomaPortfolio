8bf18784886383653a670dbef2fa1c7ec540abfa
MountainGurl <palomita@Palomas-MacBook-Air.local>
2023-10-09 20:57:52 -0400

    logo addition and nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,22 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className= "mx-auto text-3xl"> 
+    <main className="min-h-screen">
+      <header className="flex bg-gray-800 h-12 items-center px-5 justify-between">
+          <div>
+            <Image alt="paloma" src="/paloma.jpg" width={50} height={50} />
+          </div>
+          <div>
+          <svg width="35px" height="35px" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+<path d="M4 18L20 18" stroke="white" stroke-width="2" stroke-linecap="round"/>
+<path d="M4 12L20 12" stroke="white" stroke-width="2" stroke-linecap="round"/>
+<path d="M4 6L20 6" stroke="white" stroke-width="2" stroke-linecap="round"/>
+</svg>
+          </div>
+        </header>
+      <div className= "font-mono text-sm h-full">
+        
+        <h1 className= "mx-auto text-3xl w-fit py-10"> 
           {"Paloma Land"}
         </h1>
       </div>
